2a626aa0eb522a7f1d03a644ed67a938a150863c
EbrahemElbadry11 <beboelbadry85@gmail.com>
2026-02-23 21:45:18 +0200

    fix direction of admin page

--- assets/js/Auth/login.js
+++ assets/js/Auth/login.js
@@ -114,7 +114,7 @@ loginForm.addEventListener("submit", async (e) => {
 
     setTimeout(() => {
       if (user.role === "admin") {
-        window.location.href = "../../../pages/admin/users.html";
+        window.location.href = "../../../pages/admin/dashboard.html";
       } else if (user.role === "seller") {
         window.location.href = "../../../pages/seller/html/seller-dashboard.html";
       } else {
